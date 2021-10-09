0bd4a66266f7d77070939f3a9302e01371aba7cf
gregoriofracassi <gregorio.fracassi@gmail.com>
2021-10-09 09:34:30 +0200

    improved search route

--- src/services/search/index.js
+++ src/services/search/index.js
@@ -12,26 +12,29 @@ const searchRouter = Router()
 
 searchRouter.get("/", JWTAuthMiddleware, async (req, res, next) => {
   try {
-    console.log(req.user)
     const result = {
       uni: { notes: [], sessions: [], users: [] },
       other: { notes: [], sessions: [], users: [] },
     }
     const queryObj = q2m(req.query)
     console.log(queryObj)
+
+    // ------------ search through subjects ------------
     const querySubject = await SubjectModel.findOne({
-      subject: queryObj.criteria.key,
+      subject: new RegExp(queryObj.criteria.key, "i"),
     })
     if (querySubject) {
-      const allTutorSessions = await TutorSessionsModel.find({
-        subject: querySubject._id,
+      const usersWithSubj = await UserModel.find({
+        availableSubjects: querySubject._id,
       })
-      if (allTutorSessions.length !== 0) {
-        allTutorSessions.forEach((sess) => {
-          if (sess.uni === req.user.uni) {
-            result.uni.sessions.push(sess)
-          } else {
-            result.other.sessions.push(sess)
+      if (usersWithSubj.length !== 0) {
+        usersWithSubj.forEach((user) => {
+          if (user._id.toString() !== req.user._id.toString()) {
+            if (user.uni.toString() === req.user.uni.toString()) {
+              result.uni.sessions.push(user)
+            } else {
+              result.other.sessions.push(user)
+            }
           }
         })
       }
@@ -41,32 +44,36 @@ searchRouter.get("/", JWTAuthMiddleware, async (req, res, next) => {
       })
       if (allNotes.length !== 0) {
         allNotes.forEach((notes) => {
-          if (notes.uni === req.user.uni) {
-            result.uni.notes.push(notes)
-          } else {
-            result.other.notes.push(notes)
+          if (notes.author.toString() !== req.user._id.toString()) {
+            if (notes.uni.toString() === req.user.uni.toString()) {
+              result.uni.notes.push(notes)
+            } else {
+              result.other.notes.push(notes)
+            }
           }
         })
       }
     }
 
+    // ------------- search through users -------------
     const allUsers = await UserModel.find({
       $or: [
-        { name: queryObj.criteria.key },
-        { surname: queryObj.criteria.key },
+        { name: new RegExp(queryObj.criteria.key, "i") },
+        { surname: new RegExp(queryObj.criteria.key, "i") },
       ],
     })
     if (allUsers.length !== 0) {
       allUsers.forEach((user) => {
-        if (user.uni === req.user.uni) {
-          result.uni.users.push(user)
-        } else {
-          result.other.users.push(user)
+        if (user._id.toString() !== req.user._id.toString()) {
+          if (user.uni === req.user.uni) {
+            result.uni.users.push(user)
+          } else {
+            result.other.users.push(user)
+          }
         }
       })
     }
-    console.log(result)
-    res.status(200).send("eapfj")
+    res.status(200).send(result)
   } catch (error) {
     next(createError(500, { message: error.message }))
   }
